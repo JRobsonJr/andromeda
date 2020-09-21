def63fb175a87dc6c75967e4fa8a4b57e84ff920
Robson Junior <jose.robson@splab.ufcg.edu.br>
2020-09-21 14:54:41 -0300

    Improve theme

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,6 +1,18 @@
 export const theme = {
+  fonts: {
+    body:
+      'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", sans-serif',
+    heading: 'Georgia, serif',
+    monospace: 'Menlo, monospace',
+  },
   colors: {
     background: '#FFFFFF',
   },
   fontSizes: [12, 14, 16, 18, 20, 24],
+  spaces: [0, 2, 4, 8, 16],
+  styles: {
+    root: {
+      fontFamily: 'body',
+    },
+  }
 }
